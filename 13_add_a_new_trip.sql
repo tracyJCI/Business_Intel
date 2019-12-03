4abc7df7fa89d635afa656082225a3875d3af74e
tracyJCI <49816536+tracyJCI@users.noreply.github.com>
2019-12-03 16:21:48 -0500

    SQL

---
+++ 13_add_a_new_trip.sql
@@ -0,0 +1,4 @@
+USE www;
+INSERT INTO trip(trip_number,trip_date,capacity,latest_guest_arrival_time,departure_time,
+estimated_return_time,destination_code,trip_type_code,guide_employee_id,gathering_point_id)
+VALUES(666,'2014-08-02',7,'07:30:00','08:15:00','16:00:00',2,1,8,2);
