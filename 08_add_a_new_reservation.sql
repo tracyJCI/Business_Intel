4c63a7d05118987a518a1d119713318323de7c9a
tracyJCI <49816536+tracyJCI@users.noreply.github.com>
2019-12-03 16:17:33 -0500

    SQL

---
+++ 08_add_a_new_reservation.sql
@@ -0,0 +1,6 @@
+USE www;
+
+INSERT INTO guest(guest_id,first_name,last_name,experience_code,age,weight, isSwimmer) 
+values(21, 'Lilly','Ludsen',4, 25, 120,1);
+
+INSERT INTO reservation values(562,21);
