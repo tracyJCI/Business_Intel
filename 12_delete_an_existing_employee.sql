4251911b317f26ca42070255d3e91a7bfb7b14b7
tracyJCI <49816536+tracyJCI@users.noreply.github.com>
2019-12-03 16:21:26 -0500

    SQL

---
+++ 12_delete_an_existing_employee.sql
@@ -0,0 +1,13 @@
+USE www;
+
+DELETE FROM can_guide 
+WHERE
+    employee_id = 6;
+    
+DELETE FROM plays_role 
+WHERE
+    employee_id = 6;
+    
+DELETE FROM employee 
+WHERE
+    employee_id = 6;
