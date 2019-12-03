f9ac926c003c35f9c6c73b5c1e37f69ace7047f5
tracyJCI <49816536+tracyJCI@users.noreply.github.com>
2019-12-03 16:13:37 -0500

    SQL

---
+++ 02_phone_list.sql
@@ -0,0 +1,10 @@
+USE www;
+SELECT 
+    employee_id,
+    nickname,
+    CONCAT(first_name, ' ', last_name) AS 'employee name',
+    mobile_phone,
+    home_phone
+FROM
+    employee
+ORDER BY nickname;
